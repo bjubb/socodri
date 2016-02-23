445aab881e6924dc6f8f3e80365ad36b0f7a2d7b
Brennan Jubb <brennan@socialcode.com>
2016-02-23 08:42:11 -0500

    url first match patterns

--- socodri/urls.py
+++ socodri/urls.py
@@ -3,11 +3,11 @@ from socodri import views, resources
 
 
 urlpatterns = patterns('',
-    (r'api/initiative/', include(resources.InitiativeResource.urls())),
-    (r'api/window/', include(resources.WindowResource.urls())),
-    (r'api/action/', include(resources.ActionResource.urls())),
-    (r'api/stage/', include(resources.StageResource.urls())),
-    (r'api/label/', include(resources.LabelResource.urls())),
+    (r'^api/initiative/', include(resources.InitiativeResource.urls())),
+    (r'^api/window/', include(resources.WindowResource.urls())),
+    (r'^api/action/', include(resources.ActionResource.urls())),
+    (r'^api/stage/', include(resources.StageResource.urls())),
+    (r'^api/label/', include(resources.LabelResource.urls())),
     (r'^ping/$', views.ping),
     (r'^.*/$', views.show_app),
     (r'^$', views.show_app)
